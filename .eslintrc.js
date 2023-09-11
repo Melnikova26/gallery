44689825847ccbb38812cf3c5397937a5fb14348
Melnikova26 <mmelnekowa@gmail.com>
2023-09-11 08:44:08 +0500

    fix: исправлен eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
-    extends: [
-      'airbnb',
-      'airbnb-typescript',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended-type-checked',
-      'plugin:@typescript-eslint/stylistic-type-checked',
-      'prettier',
-    ],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules: {
-        'prettier/prettier': 'warn',
-    },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "prettier/prettier": "warn",
+  },
 };
